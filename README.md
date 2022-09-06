abc805a8e4441bf1375b375d1325e0ec8c008881
zhou5101 <zhouchen234@gmail.com>
2022-09-06 10:10:06 -0400

    upload all files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Team_4_Forest
+# Cellular Automata Forest Fire
+
--- forest.py
+++ forest.py
@@ -3,47 +3,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from matplotlib import colors
+import FWI
 import random
 import copy
 
 # colors for visualization
-EMPTY, TREE, FIRE, WATER = 0, 1, 2, 3
+EMPTY, TREE, FIRE, CHAR = 0, 1, 2, 3
 colors_list = [(0.2,0,0), (0,0.5,0), (1,0,0), 'orange', 'black']
 cmap = colors.ListedColormap(colors_list)
 bounds = [0,1,2,3,4]
 norm = colors.BoundaryNorm(bounds, cmap.N)
 
 
-def windDirection(windOp, nx, ny):
+def windDirection(windOp):
   #N - 1
-  if windOp == 1:
-     return [1, 1, 1, 0, 0, 0, 0, 0]
+  if windOp == 0:
+     return [(-1,-1), (-1,0), (-1,1)]
   #S - 2
-  elif windOp == 2:
-    return [0, 0, 0, 0, 0, 1, 1, 1]
+  elif windOp == 1:
+    return [(1,-1), (1,0), (1,1)]
   #W - 3 
-  elif windOp == 3:
-    return [1, 0, 0, 1, 0, 1, 0, 0]
+  elif windOp == 2:
+    return [(-1,-1), (0,-1), (1,-1)]
   #E - 4
-  elif windOp == 4:
-    return [0, 0, 1, 0, 1, 0, 0, 1]
+  elif windOp == 3:
+    return [(-1,1), (0, 1), (1,1)]
   #NW - 5
-  elif windOp == 5:
-    return [1, 1, 0, 1, 0, 0, 0, 0]
+  elif windOp == 4:
+    return [(-1,-1), (-1,0), (0,-1)]
   #NE - 6
-  elif windOp == 6:
-    return [0, 1, 1, 0, 1, 0, 0, 0]
+  elif windOp == 5:
+    return [(-1,0), (-1,1), (0, 1)]
   #SW - 7 
-  elif windOp == 7:
-    return [0, 0, 0, 1, 0, 1, 1, 0]
+  elif windOp == 6:
+    return [(0,-1), (1,-1), (1,0)]
   #SE - 8
-  elif windOp == 8:
-    return [0, 0, 0, 0, 1, 0, 1, 1]
+  elif windOp == 7:
+    return [(0, 1), (1,0), (1,1)]
   else:
-    return [1]*8
+    return [(-1,-1), (-1,0), (-1,1), (0,-1), (0, 1), (1,-1), (1,0), (1,1)]
 
 def isValid(i, j, x, y):
-  if 0<=i<=x-1 and 0<=j<=y-1:
+  if 0<=i<x and 0<=j<y:
     return True
   else:
     return False
@@ -53,43 +54,51 @@ Parameters
 '''
 # inits 
 # density
-d = 0.6
-
+d = 0.70
 # forest size
 nX, nY = 100,100
 
 # neihborhood
 # Moore neighborhood =((-1,-1), (-1,0), (-1,1), (0,-1), (0, 1), (1,-1), (1,0), (1,1)) 
-x =  ([-1,-1,-1,0,0,1,1,1])
-y =  ([-1,0,1,-1,1,-1,0,1])
+
+'''
+The Fire Weather Index (FWI) system provides fire danger information following the European Forest Fire Information System (EFFIS) classification:
+very low (0.0 – 5.2)
+low (5.2 – 11.2)
+moderate (11.2 – 21.3)
+high (21.3 – 38.0)
+very high (38.0 – 50.0)
+extreme (50.0 – 100.0)
+'''
 
 
 # wind
-windOp = 2
-neighbor = windDirection(windOp, x, y) #return proper
+windOp = 10
+neighbor = [(-1,-1), (-1,0), (-1,1), (0,-1), (0, 1), (1,-1), (1,0), (1,1)]
+#windDirection(windOp) #return proper
 
-# humidity
+# humidity level 1-low, 2-med ,3-high
 humidity = 0
 
-# temperature
+# humidity level 1-low, 2-med ,3-high
 temp = 0
+
 '''
 Parameters
 '''
 
 def init(nX, nY, d):
-    '''
-    nX, nY: 
-    '''
     grid = np.zeros((nX,nY))
     empty = []
     tree = []
 
-    global fire
-    fire = []
-
     nEmpty = 0
     nTree = 0
+    nFire = 0
+    nChar = 0
+    treeData = []
+    fireData = []
+    charData = []
 
     for i in range(nX):
         for j in range(nY):
@@ -114,74 +123,99 @@ def init(nX, nY, d):
         t = tree.pop(i)
         grid[t[0], t[1]] = FIRE
         nTree -= 1
-        fire.append((t[0], t[1]))
+        nFire += 1
 
-    return grid
+    treeData.append(nTree)
+    fireData.append(nFire)
+    charData.append(nChar)
+    return grid, treeData, fireData, charData
 
-def spread(grid, fire, x, y, neighbor):
-    newGrid = grid.copy()
-    if len(fire) != 0: 
-        t = fire.pop(0)
-        for i in range(len(neighbor)):
-            if neighbor[i] != 0 and isValid(t[0]+x[i], t[1]+y[i], len(grid), len(grid[0])):
-                if grid[t[0]+x[i], t[1]+y[i]] == 1:
-                    newGrid[t[0]+x[i], t[1]+y[i]] = 2
-                    fire.append((t[0]+x[i], t[1]+y[i]))
-        newGrid[t[0], t[1]] = 3
-    return newGrid
 
-def update(frameNum,img, grid, fire, x, y, neighbor):
-    newGrid = spread(grid, fire, x, y, neighbor)
-    img.set_data(newGrid)
-    grid[:,:] = newGrid[:,:]
-    return img
+
+def spread(g, n, nX, nY):
+    g1 = np.zeros((nX, nY))
+    nTree = 0
+    nFire = 0
+    nChar = 0
+    for x in range(nX):
+        for y in range(nY):
+            if g[x, y] == TREE:
+                nTree+=1
+                g1[x, y] = TREE
+                for i, j in n:
+                    if x+i in range(nX) and y+j in range(nY) and g[x+i, y+j] == FIRE and random.random() > 1-0.5:
+                        g1[x, y] = FIRE
+                        nTree-=1
+                        nFire+=1
+                        break
+            if g[x, y] == FIRE:
+                nChar+=1
+                g1[x,y] = CHAR
+            if g[x, y] == EMPTY:
+                g1[x,y] = EMPTY
+            if g[x, y] == CHAR:
+                nChar+=1
+                g1[x,y] = CHAR
+
+    return g1, nTree, nFire, nChar
+
+
+def update(frameNum,img, X, neighbor,nX,nY,treeData, fireData, charData):
+    if frameNum%20 > 9:
+        neighbor = windDirection(random.randint(0,8))
+    X1, nTree, nFire, nChar = spread(X, neighbor, nX, nY)
+    n.append(frameNum)
+    treeData.append(nTree)
+    fireData.append(nFire)
+    charData.append(nChar)
+    #display = copy.deepcopy(X1)
+    img.set_data(X1)
+    X[:,:] = X1[:,:]
+    axs[1].plot(n, treeData, color="green")
+    axs[1].plot(n, fireData, color="orange")
+    axs[1].plot(n, charData, color="blue")
+    axs[1].legend(['Tree', 'Burning', 'Char'],loc='upper right')
+    return img,
 
 '''
 Animation
 '''
-grid = init(nX,nY,d)
-oldGrid = grid
+index = FWI.calcFWI(4,17,45,25,0, 85,6,15,45.98)
+grid, treeData, fireData, charData = init(nX,nY,d)
+#oldGrid = grid
+title = f'Forest_Fire_d={d}, FWI={index}, Wind Diretion Changes'
+fig, axs = plt.subplots(1, 2, figsize=(10, 5), gridspec_kw={'width_ratios': [2, 2]})
 
-fig = plt.figure(figsize=(25/3, 6.25))
-ax = fig.add_subplot(111)
-ax.set_axis_off()
-img = ax.imshow(oldGrid, cmap=cmap, norm=norm)
+axs[0].set_axis_off()
 
+img = axs[0].imshow(grid, cmap=cmap, norm=norm)
+axs[1].set_ylim(0, treeData[0])
+n=[0]
 
-# Interval between frames (ms).
+fig.suptitle(title)
+
+# Interval between frames (ms). slows down change of plot
 interval = 200
-anim = animation.FuncAnimation(fig, update, fargs=(img, oldGrid, fire, x, y, neighbor), interval=interval, frames=128)
-#anim.save("forest_fire.mp4")
+#frames -> how long will animation run
+anim = animation.FuncAnimation(fig, update, fargs=(img, grid, neighbor, nX, nY, treeData, fireData, charData), interval=interval, frames=135,blit=True)
+path = f'./{title}_2.mp4'
+#writervideo = animation.FFMpegWriter(fps=60) 
+anim.save(path, fps = 60)
+#anim.save(path, fps=30)
+#plt.show()
+
+'''
+plt.plot(n, treeData, color="green")
+plt.plot(n, fireData, color="orange")
+plt.plot(n, charData, color="blue")
+plt.legend(['Tree', 'Burning', 'Char'],loc='upper right')
+plt.title(title)
 plt.show()
-
 '''
-Animation
+'''
+#Animation
 '''
 
-'''
-def animate(i):
-    im.set_data(animate.X)
-    animate.X = iterate(animate.X)
-# Bind our grid to the identifier X in the animate function's namespace.
-animate.X = X
-def iterate(X):
-    X1 = X
-    for n in range(100):
-        if len(fire) == 0:
-            break
 
-        t = fire.pop(0)
-        for i in range(10): 
-            if isValid(t[0]+x_coor[i], t[1]+y_coord[i]) and neighbourhood[i] !=0:
-                if X[t[0]+x_coor[i],  t[1]+y_coord[i]] == TREE:
-                    X1[t[0]+x_coor[i],  t[1]+y_coord[i]] = FIRE
-                    nBurn +=1
-                    nTree -=1
-                    fire.append((t[0]+x_coor[i], t[1]+y_coord[i]))
-
-        X1[t[0],t[1]] = EMPTY
-        nChared +=1
-        nBurn-=1
-        char.append(t)
-    return X1
-'''
+#month, temperature, relative humidity, wind speed, rain,   previous FFMC, DMC, DC, and latitude
+print('Done')
